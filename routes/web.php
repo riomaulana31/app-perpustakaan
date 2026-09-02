d62bc7efc7e1d48b7b7c082b0b7419ea231e3330
Rio <rio@gmail.com>
2026-09-02 12:35:13 +0700

    [P-2] setup routing resource semua tabel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ Route::resource('members', MemberController::class);
 Route::resource('loans', LoanController::class);
 Route::put('/loans/{id}/kembalikan', [LoanController::class, 'kembalikan'])
     ->name('loans.kembalikan');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/info', function () {
+        return 'Ini adalah halaman informasi khusus Admin';
+    });
+});
